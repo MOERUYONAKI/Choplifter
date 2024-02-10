a1b2b2d8705b7c13eeb7db260053570aa10b4949
MOERUYONAKI <1bbordi1@gmail.com>
2024-02-10 15:19:11 +0100

    Update move.py

--- Moeru/move.py
+++ Moeru/move.py
@@ -13,8 +13,68 @@ shot = py.Rect(rect.left + 1, rect.top + 1, 3, 3)
 
 surface = py.display.get_surface()
 
+size = (1280, 720)
+
 while running:
     # poll for events
+    pressed = py.key.get_pressed()
+
+    if pressed[py.K_z]: # - Haut
+        rect.top = rect.top - 3
+
+        if move_id == 0:
+            shot.top = shot.top - 3
+
+        if rect.top < 0:
+            rect.top = 0
+
+            if move_id == 0:
+                shot.top = 1
+
+        last_move = 'z'
+
+    if pressed[py.K_q]: # - Gauche    
+        rect.left = rect.left - 3
+
+        if move_id == 0:
+            shot.left = shot.left - 3
+
+        if rect.left < 0:
+            rect.left = 0
+
+            if move_id == 0:
+                shot.left = 1
+
+        last_move = 'q'
+
+    if pressed[py.K_s]: # - Bas
+        rect.top = rect.top + 3
+
+        if move_id == 0:
+            shot.top = shot.top + 3
+
+        if rect.top > size[1] - 5:
+            rect.top = size[1] - 5
+
+            if move_id == 0:
+                shot.top = size[1] - 6
+
+        last_move = 's'
+
+    if pressed[py.K_d]: # - Droite
+        rect.left = rect.left + 3
+
+        if move_id == 0:
+            shot.left = shot.left + 3
+
+        if rect.left > size[0] - 5:
+            rect.left = size[0] - 5
+
+            if move_id == 0:
+                shot.top = size[0] - 6
+
+        last_move = 'd'
+
     for event in py.event.get():
         if event.type == py.QUIT:
             running = False # end of the loop
@@ -22,79 +82,23 @@ while running:
         if event.type == py.KEYUP: 
             if event.key == py.K_f and not py.display.is_fullscreen(): # set fullscreen mode
                 screen = py.display.set_mode((0, 0), py.FULLSCREEN)
+                size = (py.display.Info().current_w, py.display.Info().current_h)
 
             if event.key == py.K_ESCAPE and py.display.is_fullscreen(): # set window mode
                 screen = py.display.set_mode((1280, 720))
 
-
         if event.type == py.KEYDOWN:
             if event.key == py.K_z: # Haut
-                rect.top = rect.top - 5
-
-                if move_id == 0:
-                    shot.top = shot.top - 5
-
-                if rect.top < 0:
-                    rect.top = 0
-
-                    if move_id == 0:
-                        shot.top = 1
-
-                else:
-                    print("L'utilisateur se déplace sur le haut !")
-
-                last_move = 'z'
+                print("L'utilisateur se déplace sur le haut !")
 
             if event.key == py.K_q: # Gauche
-                rect.left = rect.left - 5
-
-                if move_id == 0:
-                    shot.left = shot.left - 5
-
-                if rect.left < 0:
-                    rect.left = 0
-
-                    if move_id == 0:
-                        shot.left = 1
-
-                else:
-                    print("L'utilisateur se déplace sur la gauche !")
-
-                last_move = 'q'
+                print("L'utilisateur se déplace sur la gauche !")
 
             if event.key == py.K_s: # Bas
-                rect.top = rect.top + 5
-
-                if move_id == 0:
-                    shot.top = shot.top + 5
-
-                if rect.top > 720:
-                    rect.top = 720
-
-                    if move_id == 0:
-                        shot.top = 719
-
-                else:
-                    print("L'utilisateur se déplace sur le bas !")
-
-                last_move = 's'
+                print("L'utilisateur se déplace sur le bas !")
 
             if event.key == py.K_d: # Droite
-                rect.left = rect.left + 5
-
-                if move_id == 0:
-                    shot.left = shot.left + 5
-
-                if rect.left > 1280:
-                    rect.left = 1280
-
-                    if move_id == 0:
-                        shot.top = 1279
-
-                else:
-                    print("L'utilisateur se déplace sur la droite !")
-
-                last_move = 'd'
+                print("L'utilisateur se déplace sur la droite !")
 
         if event.type == py.MOUSEBUTTONUP:
             if last_move == 'z' and move_id == 0:
@@ -119,7 +123,7 @@ while running:
     # RENDER YOUR GAME HERE
     if move_id !=0:
         if move_id == 1: # mouvement du tir (haut)
-            shot.top = shot.top - 3
+            shot.top = shot.top - 5
 
             if shot.top <= 0:
                 move_id = 0
@@ -128,7 +132,7 @@ while running:
                 print("Le tir est de nouveau prêt !")
         
         if move_id == 2: # mouvement du tir (gauche)
-            shot.left = shot.left - 3
+            shot.left = shot.left - 5
 
             if shot.left <= 0:
                 move_id = 0
@@ -137,18 +141,18 @@ while running:
                 print("Le tir est de nouveau prêt !")
 
         if move_id == 3: # mouvement du tir (bas)
-            shot.top = shot.top + 3
+            shot.top = shot.top + 5
 
-            if shot.top >= 720:
+            if (shot.top >= size[1]):
                 move_id = 0
                 shot.left = rect.left + 1
                 shot.top = rect.top + 1
                 print("Le tir est de nouveau prêt !")
 
         if move_id == 4: # mouvement du tir (droite)
-            shot.left = shot.left + 3
+            shot.left = shot.left + 5
 
-            if shot.left >= 1280:
+            if (shot.left >= size[0]):
                 move_id = 0
                 shot.left = rect.left + 1
                 shot.top = rect.top + 1
