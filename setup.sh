fb5a0dd902491ab25c417bde0d2ab2ee25878c34
MOERUYONAKI <1bbordi1@gmail.com>
2024-05-22 13:56:40 +0200

    setup update

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# Setup for GNU/Linux
+
+#!/bin/bash
+
+python3.9 -m PyInstaller --onefile main.py
+python3.9 -m PyInstaller --onefile choplifter.py
+python3.9 -m PyInstaller --onefile choplifter_survival.py
+python3.9 -m PyInstaller --onefile init/assets.py
+python3.9 -m PyInstaller --onefile init/background.py
+python3.9 -m PyInstaller --onefile init/chop.py
+python3.9 -m PyInstaller --onefile init/tank.py
+python3.9 -m PyInstaller --onefile init/jet.py
+python3.9 -m PyInstaller --onefile init/alien.py
