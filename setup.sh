1df8cdd2384ed077362e8c629c827b3b50dc41f5
Moeru <105922034+MOERUYONAKI@users.noreply.github.com>
2024-05-28 09:42:30 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 
 #!/bin/bash
 
-python3.9 -m PyInstaller --onefile main.py
-python3.9 -m PyInstaller --onefile choplifter.py
-python3.9 -m PyInstaller --onefile choplifter_survival.py
-python3.9 -m PyInstaller --onefile init/assets.py
-python3.9 -m PyInstaller --onefile init/background.py
-python3.9 -m PyInstaller --onefile init/chop.py
-python3.9 -m PyInstaller --onefile init/tank.py
-python3.9 -m PyInstaller --onefile init/jet.py
-python3.9 -m PyInstaller --onefile init/alien.py
+python3 -m PyInstaller --onefile main.py
+python3 -m PyInstaller --onefile choplifter.py
+python3 -m PyInstaller --onefile choplifter_survival.py
+python3 -m PyInstaller --onefile init/assets.py
+python3 -m PyInstaller --onefile init/background.py
+python3 -m PyInstaller --onefile init/chop.py
+python3 -m PyInstaller --onefile init/tank.py
+python3 -m PyInstaller --onefile init/jet.py
+python3 -m PyInstaller --onefile init/alien.py
 
 cd dist
 main.exe
