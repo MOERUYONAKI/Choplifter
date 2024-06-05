f1dd32645e1fffe4a7b7bc0fe1f05cb05f58515f
MOERUYONAKI <1bbordi1@gmail.com>
2024-06-05 14:23:06 +0200

    Update setup.ps1

--- Windows/setup.ps1
+++ Windows/setup.ps1
@@ -4,7 +4,7 @@ python3 -m PyInstaller --onefile main.py
 python3 -m PyInstaller --onefile choplifter.py
 python3 -m PyInstaller --onefile choplifter_survival.py
 python3 -m PyInstaller --onefile init/assets.py
-python3 -m PyInstaller --onefile init/background.py
+python3 -m PyInstaller --onefile --add-data "assets\\bg1.png;." --add-data "assets\\bg2.png;." --add-data "assets\\bg3.png;." init/background.py
 python3 -m PyInstaller --onefile init/chop.py
 python3 -m PyInstaller --onefile init/tank.py
 python3 -m PyInstaller --onefile init/jet.py
