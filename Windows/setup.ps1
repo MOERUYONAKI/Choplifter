a9e4884148ffcf308bc27eb4cd029c6fe5547ddf
Kevin-Sawyer <127890218+Kevin-Sawyer@users.noreply.github.com>
2024-06-05 14:47:41 +0200

    Update setup.ps1

--- Windows/setup.ps1
+++ Windows/setup.ps1
@@ -2,7 +2,7 @@
 
 python3 -m PyInstaller --onefile main.py
 python3 -m PyInstaller --onefile --add-data "assets\\Fonts\\amd64_microsoft-windows-f..ruetype-comicsansms_31bf3856ad364e35_10.0.22621.1_none_3deaef772e20c404\\comic.ttf" --add-data "assets\\Songs\\Audiomachine - By the Hand of the Mortal.mp3" --add-data "assets\menu_bg.jpg" --add-data "assets\\choplifter_txt.png" --add-data "assets\\play_txt.png" --add-data "assets\\quit_txt.png" choplifter.py
-python3 -m PyInstaller --onefile choplifter_survival.py
+python3 -m PyInstaller --onefile --add-data "assets\\Fonts\\amd64_microsoft-windows-f..ruetype-comicsansms_31bf3856ad364e35_10.0.22621.1_none_3deaef772e20c404\\comic.ttf" --add-data "assets\\Songs\\Audiomachine - By the Hand of the Mortal.mp3" --add-data "assets\\menu_bg.jpg" --add-data "assets\\choplifter_txt.png" --add-data "assets\\play_txt.png" --add-data "assets\\quit_txt.png" choplifter_survival.py
 python3 -m PyInstaller --onefile init/assets.py
 python3 -m PyInstaller --onefile --add-data "assets\\bg1.png;." --add-data "assets\\bg2.png;." --add-data "assets\\bg3.png;." init/background.py
 python3 -m PyInstaller --onefile --add-data "assets\\helicopter.png" --add-data "assets\\revert_helicopter.png" --add-data "assets\\helico_ball.png" --add-data "assets\\grounded_helicopter.png" --add-data "assets\\green_pewpew.png" --add-data "assets\\green_pewpew2.png" --add-data "assets\\green_revert_pewpew.png" init/chop.py
