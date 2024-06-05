0a611d035dac0b1ee70e589c9db84d275d21bade
MOERUYONAKI <1bbordi1@gmail.com>
2024-06-05 15:47:23 +0200

    setup update

--- Linux/setup.sh
+++ Linux/setup.sh
@@ -4,5 +4,4 @@
 
 python3 -m PyInstaller --onefile --add-data "*.py:." --add-data "init/*.py:init" --add-data "assets/*.png:assets" --add-data "assets/*.jpg:assets" --add-data "assets/Songs/*.mp3:assets/Songs" --add-data "assets/Fonts/*.ttf:assets/Fonts" main.py
 
-cd dist
-./main.exe
+./dist/main.exe
--- Windows/setup.ps1
+++ Windows/setup.ps1
@@ -1,5 +1,5 @@
 # Setup for windows
 
-python3 -m PyInstaller --onefile --add-data "*.py:." --add-data "init/*.py:init" --add-data "assets/*.png:assets" --add-data "assets/*.jpg:assets" --add-data "assets/Songs/*.mp3:assets/Songs" --add-data "assets/Fonts/*.ttf:assets/Fonts" main.py
+python3 -m PyInstaller --onefile --add-data "*.py:." --add-data "init/*.py:init" --add-data "assets/*.png:assets" --add-data "assets/*.jpg:assets" --add-data "assets/Songs/*.mp3:assets/Songs" --add-data "assets/Fonts/amd64_microsoft-windows-f..ruetype-comicsansms_31bf3856ad364e35_10.0.22621.1_none_3deaef772e20c404/*.ttf:assets/Fonts/amd64_microsoft-windows-f..ruetype-comicsansms_31bf3856ad364e35_10.0.22621.1_none_3deaef772e20c404" main.py
 
 Start-Process dist/main.exe
